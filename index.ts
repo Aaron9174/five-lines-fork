611d3e34189264fc155b352b872baf483f301c5f
Aaron Hebson <aarommh7@gmail.com>
2024-09-04 17:15:45 -0400

    Inlined updateTile

--- index.ts
+++ index.ts
@@ -398,13 +398,9 @@ function handleInput2(current: Input) {
 function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      updateTile(x, y);
+      map[y][x].update(x, y);
     }
   }
-}
-
-function updateTile(x: number, y: number): void {
-  map[y][x].update(x, y);
 }
 
 function createGraphics(): CanvasRenderingContext2D {
